9bb801a54397e77dfdf3fe0d17d5bebea4b975e4
ismail bohra <ismailbohra99@gmail.com>
2023-12-14 20:03:02 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ import Dashboard from "./Container/Student/Dashboard/Dashboard";
 import StudentOutlet from "./Container/Student/StudentOutlet";
 import Report from "./Container/Student/Reports/Report";
 import TeacherOutlet from "./Container/Teacher/TeacherOutlet";
+import Login from "./Container/Common/Login/login";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Landing />}></Route>
+        <Route path="/login" element={<Login />}></Route>
         <Route path="/student" element={<StudentOutlet />}>
           <Route path="dashboard" element={<Dashboard />}></Route>
           <Route path="report" element={<Report/>}></Route>
