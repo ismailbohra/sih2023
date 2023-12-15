74884c8992e55e259e6e56079b907be5909ce422
ismail bohra <ismailbohra99@gmail.com>
2023-12-16 01:37:15 +0530

    minor change

--- src/Container/Student/Dashboard/Dashboard.jsx
+++ src/Container/Student/Dashboard/Dashboard.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import Cards from '../../../Components/Cards/Cards'
+import { useNavigate } from 'react-router-dom'
 
 function Dashboard() {
+  const navigate=useNavigate()
   return (
     
     <div>
     <h1>Welcome</h1>
     <h3>For Further Analysis and Report Generation, Please Take the Test.</h3>
-    <button>Take Test</button>
+    <button onClick={()=>navigate('../test')}>Take Test</button>
   </div>
   )
 }
